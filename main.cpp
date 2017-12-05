4431e56b394cb3e16adbc6aec25367ff17968188
Yubing Xie <xieyubin@Umich.edu>
2017-12-05 10:48:38 -0500

    asda

--- main.cpp
+++ main.cpp
@@ -9,10 +9,40 @@ void VSstopFunc()
 	cin >> a;
 }
 
+class State
+{
+public:
+	int x;
+	int y;
+	double utility;
+
+	State(int col, int row, double utilityVal)
+	{
+		x = col;
+		y = row;
+		utility = utilityVal;
+	}
+
+	void setU(double val)
+	{
+		utility = val;
+	}
+
+	double getU()
+	{
+		return utility;
+	}
+
+};
+
 int main(void)
 {
 
+	//map of state (x, y) -->U
 	cout << "asd" << endl;
+	//1 value iteration to build the map with Utility
+	//2 given the R(s) and y, calculate the policy
+
 
 	return 0;
 }
