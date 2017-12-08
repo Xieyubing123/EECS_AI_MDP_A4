2d9bf993d2211e56d2625c7b7f2e14ec5d3ff191
Yubing Xie <xieyubin@Umich.edu>
2017-12-08 16:42:52 -0500

    add new file

--- main.cpp
+++ main.cpp
@@ -14,9 +14,9 @@ int main(void)
     cout << "\n**********Problem 2**********\n\n" << endl;
     doProblem2(10);
     cout << "\n" << endl;
-    //doProblem2(100);
+    doProblem2(100);
     cout << "\n" << endl;
-    //doProblem2(1000);
+    doProblem2(1000);
 
     cout << "\n**********Problem 3**********\n\n" << endl;
     doProblem3();
